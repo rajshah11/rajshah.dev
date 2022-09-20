b777ac1dec54f26f2bc074bffbf3a1e5992d8cb0
Raj Shah <rajshah11@users.noreply.github.com>
2022-09-19 20:00:55 -0700

    Initial website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,46 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+<link href="https://fonts.googleapis.com/css2?family=Recursive&display=swap" rel="stylesheet"></link>
+
+const GlobalStyle = createGlobalStyle`
+
+@font-face{
+  font-family: 'Recursive';
+  src: url('https://fonts.googleapis.com/css2?family=Recursive&display=swap');
+}
+
+html{
+  background: #05445E;
+  height: 100%;
+  max-width: 60%;
+  margin:0 auto;
+  padding: 0;
+}
+
+body{
+  font-family: 'Recursive', monospace;
+  font-size: 20px;
+  color: white;
+  padding: 5% 5% 5% 5%;
+}
+
+a{
+  color: white;
+}
+`;
+
+const theme = {
+
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
 }
 
 export default MyApp
