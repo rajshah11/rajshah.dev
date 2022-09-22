e61e59a82b1fda6748b748374e5d3cbafb42edb3
Raj Shah <rajshah11@users.noreply.github.com>
2022-09-21 20:34:59 -0700

    Website 2.0 - Custom Document + Refactor

---
+++ layout/IndexLayout.tsx
@@ -0,0 +1,41 @@
+import styled from "../types/styled-components";
+import React from 'react'
+
+const StyledIndex = styled.div`
+  margin: auto;
+  padding: 2vmin;
+  width: min(max(480px, 60%), 100vw);
+  font-family: 'Recursive', sans-serif;
+  font-variation-settings: 'wght' 800;
+  font-size: 20px;
+  font-size: min(max(calc(16px + (24 - 16) * ((100vw - 500px) / (3000 - 500))), 12px), 36px);
+  padding: 2vmin;
+  background-image: linear-gradient(
+    180deg,
+    hsl(120deg 84% 56%) 0%,
+    hsl(120deg 77% 53%) 20%,
+    hsl(120deg 71% 50%) 29%,
+    hsl(120deg 74% 47%) 36%,
+    hsl(120deg 79% 43%) 43%,
+    hsl(120deg 84% 40%) 50%,
+    hsl(148deg 100% 37%) 57%,
+    hsl(162deg 100% 36%) 64%,
+    hsl(174deg 100% 36%) 71%,
+    hsl(182deg 100% 35%) 80%,
+    hsl(187deg 83% 40%) 100%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`
+
+type IndexLayoutProps = {
+  children: React.ReactNode
+}
+
+const IndexLayout = ({ children }: IndexLayoutProps) => (
+  <StyledIndex>
+    {children}
+  </StyledIndex>
+);
+
+export default IndexLayout;
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
-
+import { Html, Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
@@ -26,5 +26,21 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Recursive:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
