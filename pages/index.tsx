4fe1226cdc98bcf9ea8bd4672f53f1ec040eb879
Raj Shah <rajshah11@users.noreply.github.com>
2022-09-21 23:31:28 -0700

    Fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,8 @@
 import { IconsRow } from "../components/icons";
 import IndexLayout from "../layout/IndexLayout";
-import Head from "next/head";
 
-export default () => (
+const IndexPage = () => (
   <>
-    <Head>
-      <title>Raj Shah</title>
-      <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png" />
-      <link rel="manifest" href="/public/site.webmanifest" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-      <link href="https://fonts.googleapis.com/css2?family=Recursive:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
-    </Head>
     <IndexLayout>
 
       <h1>Hi, I'm Raj</h1>
@@ -21,9 +11,11 @@ export default () => (
 
       <h3>Before joining Stripe, I worked at Microsoft for 4 years, where I worked on APIs and systems to help customers manage their Azure subscriptions, and monitor their costs and billing.</h3>
 
-      <h4> I am also interested in the world of financial markets, football (the one that you play with your foot) and chess.</h4>
+      <h4>I am also interested in the world of financial markets, football (the one that you play with your foot) and chess.</h4>
 
       <IconsRow />
     </IndexLayout>
   </>
 );
+IndexPage.displayName = 'Index';
+export default IndexPage;
