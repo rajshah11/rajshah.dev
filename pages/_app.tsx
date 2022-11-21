345c48448d0d56beed1d505c8df28282e5987304
Raj Shah <rajshah11@users.noreply.github.com>
2022-11-21 00:19:43 -0800

    Fix CSS error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from "next/app";
 import Layout from "../layout/Layout";
 import { MDXProvider } from "@mdx-js/react";
 import { ImageGallery } from "../layout/StyledLayouts";
+import "../public/styles/flexboxgrid.css";
+import "../public/prism/prism.css";
 
 const components = { ImageGallery };
 
--- pages/_document.js
+++ pages/_document.js
@@ -14,8 +14,6 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Recursive:wght@300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         ></link>
-        <link href="/prism/prism.css" rel="stylesheet" />
-        <link href="/styles/flexboxgrid.css" rel="stylesheet" />
       </Head>
       <body>
         <Main>
