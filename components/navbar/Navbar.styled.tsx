4c4b99f73304fff3ca3ab634a8480a15fdbaf8d2
Raj Shah <rajshah11@users.noreply.github.com>
2022-11-24 21:34:17 -0800

    Fix hook

--- components/navbar/Navbar.styled.tsx
+++ components/navbar/Navbar.styled.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import styled from "styled-components";
 
-const getCurrentPath = () => {
+const useCurrentPath = () => {
   return useRouter().pathname;
 };
 
@@ -24,7 +24,7 @@ export const NavigationItem = styled.li`
 export const NavigationLink = styled(Link)`
   text-decoration: none;
   color: ${(props) =>
-    getCurrentPath() === props.href
+    useCurrentPath() === props.href
       ? props.theme.colors.lowContrastText
       : props.theme.colors.highContrastText};
 `;
