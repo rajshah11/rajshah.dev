3b3889e733c741eea9777ccd9cd9d66ba79526ed
Raj Shah <rajshah11@users.noreply.github.com>
2022-11-26 10:11:32 -0800

    Fix About section

---
+++ layouts/about-layout/AboutLayout.styled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { Layout } from "../layout/Layout.styled";
+
+export const AboutContent = styled.div`
+  font-variation-settings: "wght" 300;
+  font-size: 1.25em;
+  p {
+    font-variation-settings: "wght" 300;
+  }
+`;
--- layouts/about-layout/AboutLayout.tsx
+++ layouts/about-layout/AboutLayout.tsx
@@ -1,11 +1,12 @@
 import { Header } from "../../components/text/Text";
 import { Layout } from "../layout/Layout";
+import * as Styled from "../about-layout/AboutLayout.styled";
 
 export const AboutLayout = ({ children }: { children: React.ReactNode }) => (
   <Layout
     pageHeader={<Header margin={"0em 0em 1em 0em"}>About</Header>}
     layout="about"
   >
-    {children}
+    <Styled.AboutContent>{children}</Styled.AboutContent>
   </Layout>
 );
